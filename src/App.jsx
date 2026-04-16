7988f42281f8db7aa29e17398322dadc18f538dd
kartikDas074 <kartikdas209774@gmail.com>
2026-04-16 19:33:21 +0600

    Primary Setup For the Project Keen kepper

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+       <div className=' text-2xl'>
+        teri ma ki
+       </div>
+       <button className='btn'> botn</button>
+    </>
+  )
+}
+
+export default App
